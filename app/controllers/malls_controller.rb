b81c1a47a4e3bec1c85f3daf6eae318eee41d375
evkwan <evkwan@gmail.com>
2015-11-04 23:00:45 +0800

    generated controller and views, also modified routing

---
+++ app/controllers/malls_controller.rb
@@ -0,0 +1,9 @@
+class MallsController < ApplicationController
+
+	def index
+	# Empty  method. With empty method, it directly generates the view that has been routed in routes.rb!
+	# The view must exists in views/ though
+
+	end
+
+end
