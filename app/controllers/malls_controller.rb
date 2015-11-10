04fb7c2e4388b87fd8416c9559038442dcdaab57
Ei-Vonne Kwan <evkwan@gmail.com>
2015-11-10 08:52:51 +0000

    Added seed data and displayed db objects in view

--- app/controllers/malls_controller.rb
+++ app/controllers/malls_controller.rb
@@ -1,8 +1,10 @@
 class MallsController < ApplicationController
 
 	def home
-	# Empty  method. With empty method, it directly generates the view that has been routed in routes.rb!
-	# The view must exists in views/ though
+	    # Empty  method. With empty method, it directly generates the view that has been routed in routes.rb!
+	    # The view must exists in views/ though
+	
+    	@malls = Mall.order(:name)
 
 	end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Seed should be based on your model.
+Mall.create(name: "Low Yat Plaza", 
+            location: "Kuala Lumpur", 
+            parking_bays_num: "1000", 
+            description: "IT mall")
+            
+Mall.create(name: "Sungei Wang Plaza", 
+            location: "Kuala Lumpur", 
+            parking_bays_num: "1000", 
+            description: "Lala mall")
+            
+Mall.create(name: "Lot 10", 
+            location: "Kuala Lumpur", 
+            parking_bays_num: "1000", 
+            description: "Old mall with food court")
+            
+Mall.create(name: "Fahrenheit88", 
+            location: "Kuala Lumpur", 
+            parking_bays_num: "1000", 
+            description: "Lala mall 2")
+            
+Mall.create(name: "Starhill Gallery", 
+            location: "Kuala Lumpur", 
+            parking_bays_num: "1000[uekk6fc7a5c5]/home/evkwan/Web/rubyApps/parking-mall/app/controllers/malls_controller.rb", 
+            description: "Hi-klad mall")
+            
+ Mall.create(name: "Pavillion", 
+            location: "Kuala Lumpur", 
+            parking_bays_num: "1000", 
+            description: "Hi-klad mall 2")
+            
+Mall.create(name: "KLCC", 
+            location: "Kuala Lumpur", 
+            parking_bays_num: "1000", 
+            description: "Hi-klad mall 3")            
+
